c720c52410543f9349f4a2b20670ce3ab8cd32a1
HmWhyNot <CongerVerreauxi@gmail.com>
2022-01-28 13:06:58 -0800

    Conversion

---
+++ UWUVCI/Classes/PNGTGA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWUVCI.Classes
+{
+	[Serializable]
+    public class PNGTGA
+    {
+		private string imgPath = null;
+
+		public string ImgPath 
+		{
+			get { return imgPath; }
+			set { imgPath = value;
+			}
+		}
+
+		public byte[] ImgBin { get; set; } = null;
+
+		public string extension { get; set; }
+    }
+}
---
+++ UWUVCI/UI/Windows/DownloadWait.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace UWUVCI.UI.Windows
+{
+    /// <summary>
+    /// Interaktionslogik für DownloadWait.xaml
+    /// </summary>
+
+    partial class DownloadWait : Window
+    {
+        MainViewModel mvm;
+        DispatcherTimer timer = new DispatcherTimer();
+        public DownloadWait(string doing, string msg, MainViewModel mvm)
+        {
+            try
+            {
+                if (Owner?.GetType() == typeof(MainWindow))
+                {
+                    WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                }
+            }
+            catch (Exception)
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            }
+            this.mvm = mvm;
+            InitializeComponent();
+            Key.Text = doing;
+           
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        public DownloadWait(string doing, string msg, MainViewModel mvm, bool t)
+        {
+            try
+            {
+                if (Owner?.GetType() != typeof(MainWindow))
+                {
+                    WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                }
+            }
+            catch (Exception)
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            }
+            this.mvm = mvm;
+            InitializeComponent();
+            Key.Text = doing;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            
+        }
+        private void Window_Minimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            msgT.Text = mvm.msg;
+            pb.Value = mvm.Progress;
+            if(Key.Text.Contains("Downloading Base"))
+            {
+                if(mvm.Progress < 70)
+                {
+                    mvm.Progress += 1;
+                }
+            }
+            if(mvm.Progress == 100)
+            {
+                timer.Stop();
+                Close();
+                
+            }
+        }
+        public void changeOwner(MainWindow ow)
+        {
+            Owner = ow;
+            try
+            {
+                if (Owner?.GetType() == typeof(MainWindow))
+                {
+                    WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                    ShowInTaskbar = false;
+                }
+            }
+            catch (Exception)
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            }
+        }
+
+        private void wind_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((FindResource("mvm") as MainViewModel).mw != null)
+                {
+                    (FindResource("mvm") as MainViewModel).mw.Topmost = false;
+                }
+            }
+            catch (Exception )
+            {
+
+            }
+        }
+    }
+}
